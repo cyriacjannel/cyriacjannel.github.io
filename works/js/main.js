cffceee47f5203e4a429dfa1996675aea2ca204b
Cyriac Jannel <cyriac.jannel@gmail.com>
2017-05-05 01:04:47 +0200

    Header update / Nav update

--- works/js/main.js
+++ works/js/main.js
@@ -11,32 +11,4 @@ jQuery('document').ready(function($){
 			$('.main-header_nav').removeClass('scroll');
 		}
 	});
-	if ($(window).width() < 500) {
-		$(".main-header_wrap").click(function() {
-			$(this).toggleClass('active');
-			$(".main-header_nav").toggleClass('active');
-		})
-		$(".main-header_nav a").click(function() {
-			$(".main-header_nav").toggleClass('active');
-			$(".main-header_wrap").toggleClass('active');
-		})
-	} else {
-		$(".main-header_nav").removeClass('active');
-		$(".main-header_wrap").removeClass('active');
-	}
-	$(window).resize(function() {
-		if ($(window).width() < 500) {
-			$(".main-header_wrap").click(function() {
-				$(this).toggleClass('active');
-				$(".main-header_nav").toggleClass('active');
-			})
-			$(".main-header_nav a").click(function() {
-				$(".main-header_nav").toggleClass('active');
-				$(".main-header_wrap").toggleClass('active');
-			})
-		} else {
-			$(".main-header_nav").removeClass('active');
-			$(".main-header_wrap").removeClass('active');
-		}
-	});
 });
